8ea377134454852cfaf1578be09f2b92429527e2
VincenzoDePetris <vince02@live.it>
2023-08-24 11:01:59 +0200

    r

--- js/main.js
+++ js/main.js
@@ -1,30 +1,25 @@
-// creo una funzione
-(function () {
+cronometro = setInterval(function () {
   // stabilisco divisione millisecondi, secondi, minuti, ore e giorni
   const second = 1000,
     minute = second * 60,
     hour = minute * 60,
     day = hour * 24;
-
   // creo una costante countdown che "punta" a domani alle 9:30
-  const countDown = new Date("august 24, 2023 09:30:00").getTime();
+  const countDown = new Date("august 26, 2023 09:30:00").getTime();
 
-  cronometro = setInterval(function () {
-    //creo una costante che prende il tempo da ora
-    const now = new Date().getTime(),
-      //sottraggo i millisecondi per arrivare a domani ai millisecondi correnti
-      arrivo = countDown - now;
-
-    //stampo il tutto nell'html
-    (document.getElementById("days").innerText = Math.floor(arrivo / day)),
-      (document.getElementById("hours").innerText = Math.floor(
-        (arrivo % day) / hour
-      )),
-      (document.getElementById("minutes").innerText = Math.floor(
-        (arrivo % hour) / minute
-      )),
-      (document.getElementById("seconds").innerText = Math.floor(
-        (arrivo % minute) / second
-      ));
-  }, 0);
-})();
+  //creo una costante che prende il tempo da ora
+  const now = new Date().getTime(),
+    //sottraggo i millisecondi per arrivare a domani ai millisecondi correnti
+    arrivo = countDown - now;
+  //stampo il tutto nell'html
+  (document.getElementById("days").innerText = Math.floor(arrivo / day)),
+    (document.getElementById("hours").innerText = Math.floor(
+      (arrivo % day) / hour
+    )),
+    (document.getElementById("minutes").innerText = Math.floor(
+      (arrivo % hour) / minute
+    )),
+    (document.getElementById("seconds").innerText = Math.floor(
+      (arrivo % minute) / second
+    ));
+}, 0);
